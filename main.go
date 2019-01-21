a7d34eaa6df2044e312508cf03eefedc94831803
saud maruli <uthrul@gmail.com>
2019-01-21 13:16:33 +0700

    templates

--- main.go
+++ main.go
@@ -4,15 +4,20 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/julienschmidt/httprouter"
+	"github.com/gorilla/mux"
 )
 
-func home(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+func home(w http.ResponseWriter, r *http.Request) {
+	var username string = "sauth"
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+	if username == "" {
+			fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+	} else {
+			fmt.Fprint(w, "<h1>Welcome to my awesome site, "+username"!</h1>")
+	}
 }
 
-func contact(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:sauthrully@gmail.com\">sauthrully@gmail.com</a>.")
 }
@@ -20,13 +25,13 @@ func contact(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 func notFound(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	w.WriteHeader(http.StatusNotFound)
-	fmt.Fprint(w, "<h1>sorry :( 404 </h1>")
+	fmt.Fprint(w, "<h1>Sorry :( 404</h1>")
 }
 
 func main() {
-	r := httprouter.New()
-	r.GET("/", home)
-	r.GET("/contact", contact)
-	r.NotFound = http.HandlerFunc(notFound)
+	r := mux.NewRouter()
+	r.NotFoundHandler = http.HandlerFunc(notFound)
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
 	http.ListenAndServe(":3000", r)
 }
