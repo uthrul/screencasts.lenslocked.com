4a008dcd47228350a339961a7199eb5765b8536f
saud maruli <uthrul@gmail.com>
2019-01-24 08:19:36 +0700

    wrote a rough first draft of code the parse the signup form

--- controllers/users.go
+++ controllers/users.go
@@ -17,6 +17,7 @@ type Users struct {
 	NewView *views.View
 }
 
+// GET / signup
 func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 	// click sign up on web
 	fmt.Println("Inside the New method!!!")
@@ -25,6 +26,14 @@ func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// POST / signup
 func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "this bug")
+	if err := r.ParseForm(); err != nil {
+		panic(err)
+	}
+	// r.PostForm = map[string][]string
+	fmt.Fprintln(w, r.PostForm["email"])
+	fmt.Fprintln(w, r.PostFormValue("email"))
+	fmt.Fprintln(w, r.PostForm["password"])
+	fmt.Fprintln(w, r.PostFormValue("password"))
 }
